6ac83d398471a84fdfc65cce361ced7fbc4fff3c
entamack1511 <entamack1511@gmail.com>
2025-04-13 16:36:23 -0400

    populated the beverages data

---
+++ data/beverage.js
@@ -0,0 +1,231 @@
+
+const beverages=[
+    {
+        "Berry_Chocolate_Protein_Smoothie":
+        [   
+            {
+            ingredients: [
+                "6 fluid ounces almond milk, such as MALK®",
+                "3 ounces frozen strawberries",
+                "2 ounces frozen blueberries",
+                "1 scoop chocolate protein powder, such as Premier® or Six Star®",
+                "sugar-free whipped topping (optional)",
+                "sugar free chocolate chips"
+
+            ],
+            instructions: [
+                "Add almond milk, strawberries, and blueberries to a high-powered blender, such as a Vitamix®. Blend on High for 45 seconds. Turn blender off; add protein powder. Blend for 15 seconds more. ",
+                "Pour mixture into a glass, top with sugar-free whipped topping and sugar-free chocolate chips."
+            ],
+            url: "https://www.allrecipes.com/thmb/CGSjDR-aiDVex5vuO2_h3NqGKEs=/0x512/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/11711428_Berry-Chocolate-Protein-Smoothie_TheDailyGourmet_4x3-9ecd31aa7a96434da3b86e6745d005f1.jpg",
+            source: "TheDailyGourmet"
+            }
+        ]
+    },
+    {
+        "Passion_Fruit_Mojito":
+        [   
+            {
+            ingredients: [
+                "1 fluid ounce lime juice",
+                "1 1/2 fluid ounces simple syrup",
+                "8 mint leaves",
+                "2 fluid ounces white rum",
+                "2 cups ice cubes, or as needed",
+                "2 ounces frozen passion fruit pulp (with or without seeds, thawed)",
+                "2 fluid ounces club soda"
+            ],
+            instructions: [
+                "In a cocktail shaker, combine lime juice, simple syrup and 6 mint leaves. Use a muddler to crush the leaves for about 10 seconds. Add rum and 1 cup ice. Seal shaker and shake vigorously until outside is frosted, 10 to 15 seconds.",
+                "Strain mojito into a glass. Add passion fruit pulp and club soda and gently stir. Fill glass with crushed ice and garnish with remaining mint leaves."
+            ],
+            url: "https://www.allrecipes.com/thmb/KEbsAoDpwBzsMyOEvuCoUN-UPlg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/8774704_Passion-Fruit-Mojito_France-Cevallos_4x3-9198ba0b2aad4d6db0d26055f8a53740.jpg",
+            source:"France Cevallos"
+            }
+            
+        ]
+    },
+    {
+        "Limoncello_Sunrise":
+        [   
+            {
+            ingredients: [
+                "2 fluid ounces limoncello",
+                "4 fluid ounces orange juice",
+                "1 splash grenadine",
+                "ice cubes (optional)",
+                "1 orange slice (optional)",
+                "1 maraschino cherry (optional)"
+            ],
+            instructions: [
+                "Add ice cubes to an Old Fashioned glass.", 
+                "Pour in limoncello and orange juice.", 
+                "Slowly pour in grenadine (It will sink to the bottom of the glass).", 
+                "Garnish with an orange slice and a maraschino cherry. Serve immediately."],
+            url: "https://www.allrecipes.com/thmb/HXJP-tFI82D9WFeDJ6Q8kp0fggs=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/limoncellosunrisetheDailyGourmet4x3-02d1e0d17f40444e90d78944dd1ad5d5.jpg",
+            source: " TheDailyGourmet "
+            }
+        ]
+    },
+    {
+        "Banana_Caramel_Kefir_Smoothie":
+        [   
+            {
+            ingredients: [
+                "2 peeled frozen bananas, cut into chunks",
+                "2 cups plain unsweetened kefir (such as Lifeway®)",
+                "3 tablespoons caramel syrup",
+                "1 teaspoon vanilla extract"
+            ],
+            instructions: [
+                "Add bananas, Kefir, caramel syrup, and vanilla to a blender, and process until smooth. Pour into glasses and serve immediately."
+            ],
+            url: "https://www.allrecipes.com/thmb/ti5KfCN8CObJ6FpwjN0xC7vWGDc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/11706351_Banana-Caramel-Kefir-Smoothie_Pat-Bernitt_4x3-d4ac505f4f0949dc90a9ad61b9de870d.jpg",
+            source: "Pat Bernitt"
+            }
+        ]
+    },
+    {
+        "Orange_Creamsicle_Shake":
+        [   
+            {
+            ingredients: [
+                "1 cup freshly-squeezed orange juice",
+                "1/2 cup sugar-free French Vanilla creamer, or as needed",
+                "3/4 cup sugar-free whipped cream, such as Zero Sugar Reddi Wip®"
+            ],
+            instructions: [
+                "Pour orange juice into the wells of an ice cube tray, and freeze until solid, about 4 hours.",
+                "Place orange ice cubes and French vanilla creamer in a blender and blend until smooth. If too thick, add additional creamer, 1 tablespoon at a time.",
+                "Pour 1/3 of orange mixture into a glass. Top with 1/4 cup whipped cream. Repeat layers twice. Serve immediately."
+            ],
+            url: "https://www.allrecipes.com/thmb/srpcG3orhvchjKlanxEkqrl42BE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/11697327_Orange-Creamsicle-Shake_Yolanda-Gutierrez_4x3-3d5be23077714affb79503da5c24a55e.jpg",
+            source: "Yolanda Gutierrez"
+            }
+        ]
+    },
+    {
+        "Pomegranate_Fizz":
+        [   
+            {
+            ingredients: [
+                "1 lime wedge",
+                "5 fresh basil leaves",
+                "1 tablespoon agave syrup or honey",
+                "1 fluid ounce fresh lime juice",
+                "3 fluid ounces pomegranate juice",
+                "4 fluid ounces club soda",
+                "ice cubes, as needed"
+            ],
+            instructions: [
+                "In a shaker, add basil leaves with lime juice and agave syrup and mash with a cocktail muddler. Add pomegranate juice to the shaker and shake well.",
+                "Fill a glass with ice cubes and strain shaker contents into the glass. Top with club soda and garnish with lime wedge."
+            ],
+            url: "https://www.allrecipes.com/thmb/Sgpp2Ejph1nj8Zj5MNz2LF5uEt4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/8724216_Pomegranate-Fizz_France-Cevallos_4x3-1dbf87bcfa9f4e76b69d9cc032080563.jpg",
+            source:"France Cevallos"
+            }
+        ]
+    },
+    {
+        "Honey_Lemon_Ginger_Tea":
+        [   
+            {
+            ingredients: [
+                "4 cups water",
+                "¾ cup brown sugar",
+                "¼ cup grated ginger root",
+                "3 tea bags",
+                "2 lemons, juiced",
+                "3 tablespoons honey"
+            ],
+            instructions: [
+                "Stir water, brown sugar, and grated ginger root together in a saucepan; bring to a boil, then reduce the heat to medium-low and cook at a simmer for 20 minutes.",
+                "Remove from the heat and add tea bags. Steep tea to desired strength, 3 to 5 minutes. Remove and discard tea bags.",
+                "Stir lemon juice and honey into the tea, then strain into a pitcher."
+            ],
+            url: "https://www.allrecipes.com/thmb/KFrFI193rSOEEOJRkgHAxCDc6GQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1565212-e377cbbfdc7547f4a130906d602b2372.jpg",
+            source: "michellebelle61"
+            }
+        ]
+    },
+    {
+        "Vintage_Lemonade":
+        [   
+            {
+            ingredients: [
+                "5 lemons",
+                "1 ¼ cups white sugar",
+                "1 ¼ quarts water"
+            ],
+            instructions: [
+                "Peel the rinds from the 5 lemons and cut them into 1/2 inch slices. Set the lemons aside.",
+                "Place the rinds in a bowl and sprinkle the sugar over them. Let this stand for about one hour, so that the sugar begins to soak up the oils from the lemons.",
+                "Bring water to a boil in a covered saucepan and then pour the hot water over the sugared lemon rinds. Allow this mixture to cool for 20 minutes and then remove the rinds.",
+                "Squeeze the lemons into another bowl. Pour the juice through a strainer into the sugar mixture. Stir well, pour into pitcher and pop it in the fridge! Serve with ice cubes."
+            ],
+            url: "https://www.allrecipes.com/thmb/c_m5hm-nhFkwAZcfrJHO43UT0bs=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/445156_Vintage-Lemonade-4x3-195c79927325479bb7848ece5cab897f.jpg",
+            source: "Aunt Suzie"
+            }
+        ]
+    },
+    {
+        "Carrot_and_Apple_Juice":
+        [   
+            {
+            ingredients: [
+                "4 carrots, trimmed",
+                "2 apples, quartered",
+                "2 stalks celery",
+                "1 (1/2 inch) piece fresh ginger"
+            ],
+            instructions: [
+                "Run carrots, apples, and celery through a juicer (alternating carrot, apple, and celery) according to manufacturer's instructions. Add ginger to juicer and process."
+            ],
+            url: "https://www.allrecipes.com/thmb/t1XIKuZEaiQxF8J1n7UPA3UesIM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/2891296-carrot-and-apple-juice-Jenny-Aleman-de-Bolanos-4x3-1-9430ee45466f4201b55278ed8ca59a43.jpg",
+            source: "xprincessx"
+            }
+        ]
+    },
+    {
+        "Salted_Grapefruit_Fizz":
+        [   
+            {
+            ingredients: [
+                "1 cup grapefruit juice",
+                "1/2 cup lime juice",
+                "4 cups club soda",
+                "citrus ice ring (optional)"
+            ],
+            instructions: [
+                "To make the salted grapefruit cordial: Remove strips of zest from 1 ruby grapefruit with a vegetable peeler, avoiding any white pith. Stir grapefruit zest and sugar together in a jar. Chill, covered, for 24 hours.",
+                "Squeeze juice from the grapefruit and measure out 1/2 cup juice. Pour 1/2 cup grapefruit juice, fresh lime juice, and water over the sugar mixture in the jar. Stir in kosher salt. Stir or shake mixture to dissolve sugar. Strain out zest; chill for 1 hour or cover and keep chilled for up to 3 days.",
+                "To make the punch: Combine salted grapefruit cordial, grapefruit juice, and lime juice in a punch bowl. Chill until ready to serve; pour in club soda before serving with citrus ice ring, if using.",
+                "To make a citrus ice ring: Fill an ice ring mold with very thin half slices of grapefruit and very thin slices of lime. Fill ring halfway with water; freeze until firm, about 30 minutes. Add more water to fill mold, freeze until firm, about 2 hours."
+            ],
+            url: "https://www.allrecipes.com/thmb/qlpQlA_gGVz3_r5nzXDLzmoB9Ig=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/8717911-Salted-Grapefruit-Fizz-Allrecipes-Magazine-4x3-bb6b84611a3e43a89de54a80c6ead257.jpg",
+            source: "Juliana Hale"
+            }
+        ]
+    },
+    {
+        "Irish_Cream_Coffee_Creame":
+        [   
+            {
+            ingredients: [
+                "1 1/4 cups heavy cream",
+                "1 cup white sugar",
+                "1 teaspoon cocoa powder",
+                "4 teaspoons instant espresso",
+                "1 tablespoon vanilla extract",
+                "1 teaspoon almond extract",
+                "1/4 teaspoon kosher salt",
+                "1 cup half-and-half"],
+            instructions: [
+                "Heat heavy cream and sugar together in a small saucepan over medium heat, stirring occasionally until sugar dissolves. Whisk in cocoa powder, espresso, vanilla, almond extract and salt. Remove from heat and cool slightly. Stir in half-and-half and refrigerate until ready to serve."
+            ],
+            url: "https://www.allrecipes.com/thmb/zrKtWk2ReMpS8p4MLSI84vdAYL8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/8757830_Irish-Cream-Coffee-Creamer_Dotdash-Meredith-Food-Studios_4x3-74e5bc15d85b452ca8a7d99a3cbc672c.jpg",
+            source: "Nicole McLaughlin"
+            }
+        ]
+    },
+]
